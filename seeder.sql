dc31bf1f1500e0bb1ce2f106ca1f7641d7fbb5f0
susanlin0705 <susan.lin0705@gmail.com>
2019-08-29 14:23:29 -0500

    added seeder and create with hash password

--- seeder.sql
+++ seeder.sql
@@ -3,7 +3,9 @@ USE adlister_db;
 INSERT INTO users(username, email, password)
 VALUES ('mjohnson', 'mjohnson@gmail.com', 'pass' ),
        ('slin', 'slin@yahoo.com', 'pass'),
-       ('asifuentes', 'asifuentes@hotmail.com', 'pass');
+       ('asifuentes', 'asifuentes@hotmail.com', 'pass'),
+       ('david','david@david.com','$2a$12$trJIIiS2rBIlxf/wUqT/7emvyuaMUipzXrXk5byFsgP.S0GjbLvy6'),
+       ('admin','admin@gmail.com','$2a$12$trJIIiS2rBIlxf/wUqT/7emvyuaMUipzXrXk5byFsgP.S0GjbLvy6');
 
 INSERT INTO ads(user_id, title, description)
 VALUES (1, 'New Car', 'Brand new Honda, no miles'),
--- src/main/java/com/codeup/adlister/util/Password.java
+++ src/main/java/com/codeup/adlister/util/Password.java
@@ -5,6 +5,8 @@ import org.mindrot.jbcrypt.BCrypt;
 public class Password {
     private static final int ROUNDS = 12;
 
+
+
     public static String hash(String password) {
         return BCrypt.hashpw(password, BCrypt.gensalt(ROUNDS));
     }
